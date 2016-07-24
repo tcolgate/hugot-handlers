8b2dda2e45c8f0f5109d20163eb113ef5e0006f7
Tristan Colgate <tcolgate@gmail.com>
2016-07-24 08:52:17 +0100

    Some experiments with alert manager

--- cmd/hugot-demo/docker_run.sh
+++ cmd/hugot-demo/docker_run.sh
@@ -2,11 +2,18 @@
 set -x
 env
 
+ifconfig -a
+ip link
+ip addr
+
 sed -i -e "s/PORT/${PORT}/" /conf/alertmanager.yml
-alertmanager -config.file /conf/alertmanager.yml &
+AMDDIR=$(mktemp -d)
+alertmanager -config.file /conf/alertmanager.yml -storage.path $AMDDIR &
 
-mkdir -f /tmp/data
-rm -rf /tmp/data/*
+DDIR=$(mktemp -d)
+rm -rf ${DDIR}/*
+prometheus -config.file /conf/prometheus.yml -alertmanager.url http://localhost:9093 -storage.local.path $DDIR &
 
-prometheus -config.file /conf/prometheus.yml -alertmanager.url http://localhost:9093 -storage.local.path /tmp/data &
 hugot-demo
+
+
