8f7b3a90d024adf5f8f5396f5e3081123ad7926e
Tristan Colgate <tristan@qubit.com>
2016-11-07 10:51:03 +0000

    Try and templat ethe alerts

--- prometheus/alert.go
+++ prometheus/alert.go
@@ -1,19 +1,78 @@
 package prometheus
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"text/template"
 
+	"github.com/Masterminds/sprig"
 	"github.com/golang/glog"
 	"github.com/prometheus/alertmanager/notify"
+	"github.com/prometheus/common/model"
 	am "github.com/tcolgate/client_golang/api/alertmanager"
 	"github.com/tcolgate/hugot"
 )
+
+// These templates are stolen from Qubit's internal alertmanager configs and are rather
+// opinionated, and by no means best practice. These should probably be configurable.
+var (
+	channelTmpl = template.Must(template.New("channel").Funcs(sprig.TxtFuncMap()).Parse(`{{ if .GroupLabels.slack_channel }}{{ .GroupLabels.slack_channel }}{{ else }}DEFAULTCHAN{{ end }}`))
+	colorTmpl   = template.Must(template.New("color").Funcs(sprig.TxtFuncMap()).Parse(`{{ if eq .Status "firing" }}{{ if eq .GroupLabels.severity "page" }}danger{{else}}#ffa500{{end}}{{ else }}good{{ end }}`))
+	textTmpl    = template.Must(template.New("text").Funcs(sprig.TxtFuncMap()).Parse(`{{$caRB := .CommonAnnotations.runbook_url}}{{$caDash := .CommonAnnotations.dashboard_url}}{{ range .Alerts.Firing }}{{ printf "%s" .Annotations.description }}{{if not $caDash}}{{ if .Annotations.dashboard_url }}{{printf " <%s|:thermometer:>"  .Annotations.dashboard_url }}{{end}}{{end}}{{if not $caRB}}{{ if .Annotations.runbook_url }}{{ printf "<%s|:clipboard:>" .Annotations.runbook_url }}{{end}}{{end}}{{ printf "\n"}}{{end}}{{ if eq .Status "firing" }} {{if $caRB }}<{{ $caRB }}#{{ lower .GroupLabels.alertname }}|:clipboard:>{{ end }}{{ if $caDash }} <{{ $caDash }}|:thermometer:>{{end}}{{end}}`))
+	titleTmpl   = template.Must(template.New("title").Funcs(sprig.TxtFuncMap()).Parse(`[{{ .Status | upper }}{{ if eq .Status "firing" }}:{{ .Alerts.Firing | len }}{{ end }}] {{ .GroupLabels.alertname }} ({{ .GroupLabels.job }} in {{ .GroupLabels.cluster }})`))
+)
+
+func alertToMessage(a *model.Alert) *hugot.Message {
+	var err error
+
+	glog.Infof("ALERT DUMP %#v", *a)
+
+	color := bytes.Buffer{}
+	err = colorTmpl.Execute(&color, *a)
+	if err != nil {
+		glog.Infof("color template errors: ", err)
+		return nil
+	}
+
+	text := bytes.Buffer{}
+	err = textTmpl.Execute(&text, *a)
+	if err != nil {
+		glog.Infof("text template errors: ", err)
+		return nil
+	}
+
+	title := bytes.Buffer{}
+	err = titleTmpl.Execute(&title, *a)
+	if err != nil {
+		glog.Infof("title template errors: ", err)
+		return nil
+	}
+
+	channel := bytes.Buffer{}
+	err = channelTmpl.Execute(&channel, *a)
+	if err != nil {
+		glog.Infof("channel template errors: ", err)
+		return nil
+	}
+
+	m := hugot.Message{
+		Channel: channel.String(),
+		Attachments: []hugot.Attachment{
+			{
+				Title: title.String(),
+				Color: color.String(),
+				Text:  text.String(),
+			},
+		},
+	}
+
+	return &m
+}
 
 func (p *promH) alertsCmd(ctx context.Context, w hugot.ResponseWriter, m *hugot.Message) error {
 	if err := m.Parse(); err != nil {
@@ -31,7 +90,13 @@ func (p *promH) alertsCmd(ctx context.Context, w hugot.ResponseWriter, m *hugot.
 
 	for _, a := range as {
 		fmt.Fprintf(w, "%s: Started at %s, %#v, %#v", a.Labels["alertname"], a.StartsAt, a.Labels, a.Annotations)
+
+		m := alertToMessage(a)
+		if m != nil {
+			w.Send(ctx, m)
+		}
 	}
+
 	return nil
 }
 
@@ -56,7 +121,7 @@ func (p *promH) silencesCmd(ctx context.Context, w hugot.ResponseWriter, m *hugo
 }
 
 func (p *promH) alertsHook(w http.ResponseWriter, r *http.Request) {
-	rw, ok := hugot.ResponseWriterFromContext(r.Context())
+	_, ok := hugot.ResponseWriterFromContext(r.Context())
 	if !ok {
 		http.NotFound(w, r)
 	}
@@ -74,7 +139,7 @@ func (p *promH) alertsHook(w http.ResponseWriter, r *http.Request) {
 	// Get rid of any trailing space after decode
 	io.Copy(ioutil.Discard, r.Body)
 
-	log.Println(rw)
+	glog.Infof("Webhook dump: %#v", *hm.Data)
 	//rw.SetChannel(p.alertChan)
 	//status := strings.ToUpper(hm.Data.Status)
 	//fmt.Fprintf(rw, "%s: %#v", status, hm.Data)
