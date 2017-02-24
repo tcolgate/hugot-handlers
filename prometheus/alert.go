4badbac1099178d8ae64d956ba0b13bdea133bff
Tristan Colgate <tristan@qubit.com>
2017-02-24 10:31:24 +0000

    Try rendering alert template for prom

--- prometheus/alert.go
+++ prometheus/alert.go
@@ -8,9 +8,11 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/golang/glog"
 	"github.com/prometheus/alertmanager/notify"
+	"github.com/prometheus/common/model"
 	"github.com/tcolgate/hugot"
 	"github.com/tcolgate/hugot-handlers/prometheus/am"
 	"github.com/tcolgate/hugot/handlers/command"
@@ -20,17 +22,31 @@ func (p *promH) alertsCmd(ctx context.Context, w hugot.ResponseWriter, m *comman
 	if err := m.Parse(); err != nil {
 		return err
 	}
-	as, err := am.NewAlertAPI(p.amclient).ListGroups(ctx)
+	ags, err := am.NewAlertAPI(p.amclient).ListGroups(ctx)
 	if err != nil {
 		return err
 	}
 
-	if len(as) == 0 {
+	if len(ags) == 0 {
 		fmt.Fprint(w, "There are no outstanding alerts")
 		return nil
 	}
 
-	fmt.Fprintf(w, "%#v", as)
+	for _, ag := range ags {
+		ls := ag.Labels
+		for _, b := range ag.Blocks {
+			d := data(b.Receiver, ls, model.Alerts(b.Alerts))
+			rm, err := p.alertMessage(d)
+			if err != nil {
+				fmt.Fprintf(w, "error rendering template, %v", err)
+				continue
+			}
+
+			rm.Channel = m.Channel
+			rm.To = m.From
+			w.Send(ctx, rm)
+		}
+	}
 	return nil
 }
 
@@ -113,4 +129,94 @@ func (p *promH) alertsHook(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 	rw.Send(context.TODO(), &m)
+}
+
+type tmplData struct {
+	Receiver          string
+	Status            string
+	Alerts            model.Alerts
+	GroupLabels       map[string]string
+	CommonLabels      map[string]string
+	CommonAnnotations map[string]string
+	ExternalURL       string
+}
+
+func data(recv string, groupLabels model.LabelSet, alerts model.Alerts) *tmplData {
+	data := &tmplData{
+		Receiver:          strings.SplitN(recv, "/", 2)[0],
+		Status:            string(alerts.Status()),
+		Alerts:            alerts,
+		GroupLabels:       map[string]string{},
+		CommonLabels:      map[string]string{},
+		CommonAnnotations: map[string]string{},
+		//		ExternalURL:       t.ExternalURL.String(),
+	}
+
+	for k, v := range groupLabels {
+		data.GroupLabels[string(k)] = string(v)
+	}
+
+	if len(alerts) >= 1 {
+		var (
+			commonLabels      = alerts[0].Labels.Clone()
+			commonAnnotations = alerts[0].Annotations.Clone()
+		)
+		for _, a := range alerts[1:] {
+			for ln, lv := range commonLabels {
+				if a.Labels[ln] != lv {
+					delete(commonLabels, ln)
+				}
+			}
+			for an, av := range commonAnnotations {
+				if a.Annotations[an] != av {
+					delete(commonAnnotations, an)
+				}
+			}
+		}
+		for k, v := range commonLabels {
+			data.CommonLabels[string(k)] = string(v)
+		}
+		for k, v := range commonAnnotations {
+			data.CommonAnnotations[string(k)] = string(v)
+		}
+	}
+
+	return data
+}
+
+func (p *promH) alertMessage(d interface{}) (*hugot.Message, error) {
+	channel := bytes.Buffer{}
+	err := p.tmpls.ExecuteTemplate(&channel, "channel", d)
+	if err != nil {
+		return nil, err
+	}
+
+	title := bytes.Buffer{}
+	err = p.tmpls.ExecuteTemplate(&title, "title", d)
+	if err != nil {
+		return nil, err
+	}
+
+	titleLink := bytes.Buffer{}
+	err = p.tmpls.ExecuteTemplate(&titleLink, "title_link", d)
+	if err != nil {
+		return nil, err
+	}
+
+	color := bytes.Buffer{}
+	err = p.tmpls.ExecuteTemplate(&color, "color", d)
+	if err != nil {
+		return nil, err
+	}
+
+	m := hugot.Message{}
+	m.Channel = channel.String()
+	m.Attachments = []hugot.Attachment{
+		{
+			Title:     title.String(),
+			TitleLink: titleLink.String(),
+			Color:     color.String(),
+		},
+	}
+	return &m, nil
 }
